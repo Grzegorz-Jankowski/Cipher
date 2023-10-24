e6bb20b01ab77b87f8cffcd31cdce79cfe64266f
Grzegorz Jankowski <grzegorz.jankowski93@gmail.com>
2023-10-24 20:51:04 +0200

    FEAT(test_FileHanlder): Looking for tmp_path

--- src/tests/test_file_handler.py
+++ src/tests/test_file_handler.py
@@ -1,5 +1,6 @@
 from file_handler import FileHandler
 from buffer import Buffer
+import json
 import pytest
 
 
@@ -15,8 +16,17 @@ class TestFileHandler:
     def test_get_full_filename_path_should_show_folder_with_file_name(self, file_name, full_path):
         assert FileHandler.get_full_file_path(file_name) == full_path
 
-    def test_read_but_how_i_dont_know(self):
-        pass
+    @staticmethod
+    def test_read_when_file_exists(tmp_path):
+        file_content = {"key": "value"}
+        temp_file = tmp_path / "tempfile.json"
+        with open(temp_file, "w") as file:
+            json.dump(file_content, file)
+
+        with patch("file_handler.FileHandler.get_full_file_path", return_value=str(temp_file)):
+            FileHandler.read("tempfile.json")
+
+        assert file_content in Buffer.memory
 
     def test_save_file_but_again_how_to_do_this(self):
         pass
